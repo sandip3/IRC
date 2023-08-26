783d5bc666f5e98a717ee7067ecfea0d8aedba99
sandip3 <sm7405243902@gmail.com>
2023-08-26 17:18:46 +0530

    Upadte

---
+++ project/7/main.js
@@ -0,0 +1,20 @@
+var start = document.getElementById('str');
+var optput = document.getElementById("time-content");
+var ms = 0;
+var sec = 0;
+var min = 0;
+var timeloop = null; 
+
+function Start(on){
+        start.disabled=false;
+    timeloop = setTimeout(function ({
+        
+    })
+
+}
+function Stop(){
+
+}
+function Reset(){
+
+}
